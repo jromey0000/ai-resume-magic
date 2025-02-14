55f44ffe14dfe105ac7d0e9faa974241a8def5a2
jromey <jromey0000@gmail.com>
2025-02-13 17:02:09 -0700

    install and config axios cache interceptor

--- src/services/GlobalApi.ts
+++ src/services/GlobalApi.ts
@@ -1,11 +1,11 @@
 import { UserResume } from '@/types';
-import axios from 'axios';
+import Axios from 'axios';
+import { setupCache } from 'axios-cache-interceptor';
 
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-// const API_BASE_URL = import.meta.env;
 
-const axiosClient = axios.create({
+const axiosInstance = Axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
@@ -13,8 +13,13 @@ const axiosClient = axios.create({
   },
 });
 
+const axiosClient = setupCache(axiosInstance, {
+  debug: console.log,
+  methods: ['get'],
+});
+
 const CreateNewResume = (data: UserResume) =>
-  axiosClient.post('/user-resumes', data);
+  axiosClient.post('/user-resumes', data, { cache: false });
 
 const GetUserResumes = (userEmail: string) =>
   axiosClient.get(`/user-resumes?filters[userEmail][$eq]=${userEmail}`);
